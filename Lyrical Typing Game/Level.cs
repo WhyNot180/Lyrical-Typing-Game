1d73bff0887720556af83258e7f6d3c3b415c12d
Bence Szucs <bence.szucs200625@gmail.com>
2024-03-26 11:00:56 -0400

    Creates level class

---
+++ Lyrical Typing Game/Level.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lyrical_Typing_Game
+{
+    public class Level
+    {
+
+        private StringBuilder currentWord = new StringBuilder(32);
+
+        public string Name { get; }
+        public Level(string name) 
+        {
+            Name = name;
+
+        }
+
+
+    }
+}
