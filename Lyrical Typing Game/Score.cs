3dd30d84d45e48350e22083840b2f0880a509e17
Bence Szucs <bence.szucs200625@gmail.com>
2024-04-12 10:51:06 -0400

    Adds score class

---
+++ Lyrical Typing Game/Score.cs
@@ -0,0 +1,20 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System;
+
+namespace Lyrical_Typing_Game
+{
+    public class Score
+    {
+        [Index(0)]
+        public string PlayerName { get; set; }
+
+        [Index(1)]
+        public int HighScore { get; set; }
+
+        [Index(2)]
+        public double WordsPerMinute { get; set; }
+
+        [Index(3)]
+        public int Errors { get; set; }
+    }
+}
